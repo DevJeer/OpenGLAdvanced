cbe68ea501ae9c6b5ed0e84c89009dc3b0a6f0bd
DevJeer <1761569049@qq.com>
2020-11-01 15:32:55 +0800

    获取Shader中的变量（插槽的概念）

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,7 +1,11 @@
 #include "scene.h"
 #include "ggl.h"
+#include "utils.h"
 
 GLuint vbo;
+GLuint program;
+
+GLuint positionLocation, modelMatrixLocation, viewMatrixLocation, projectionMatrixLocation;
 void Init()
 {
 	float data[] = {
@@ -14,6 +18,27 @@ void Init()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 12, data, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// 加载Shader为绘图做准备
+	int fileSize = 0;
+	unsigned char *shaderCode = LoadFileContent("Res/test.vs", fileSize);
+	GLuint vsShader = CompileShader(GL_VERTEX_SHADER, (char*)shaderCode);
+	delete shaderCode; // 不使用之后就删掉
+
+	shaderCode = LoadFileContent("Res/test.fs", fileSize);
+	GLuint fsShader = CompileShader(GL_FRAGMENT_SHADER, (char*)shaderCode);
+	delete shaderCode;
+
+	program = CreateProgram(vsShader, fsShader);
+	glDeleteShader(vsShader);	// 删除shader，因为已经链接到program里面了
+	glDeleteShader(fsShader);
+
+	// 获取shader中的插槽位置
+	positionLocation = glGetAttribLocation(program, "position");
+	modelMatrixLocation = glGetUniformLocation(program, "ModelMatrix");
+	viewMatrixLocation = glGetUniformLocation(program, "ViewMatrix");
+	projectionMatrixLocation = glGetUniformLocation(program, "ProjectionMatrix");
+
 }
 
 void Draw()
