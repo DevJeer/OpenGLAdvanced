8fe8c29aae58c4c6dde151b9878476b628305db0
houjf <18829536923@163.com>
2020-11-02 12:59:55 +0800

    添加解码BMP格式图片的方法

--- src/utils.cpp
+++ src/utils.cpp
@@ -46,3 +46,25 @@ GLuint CreateProgram(GLuint vsShader, GLuint fsShader)
 	}
 	return program;
 }
+
+// 解码BMP格式的图片
+unsigned char* DecodeBMP(unsigned char* bmpFileData, int& width, int& height)
+{
+	// 判断图片是否是位图
+	if (0x4D42 == *((unsigned short*)bmpFileData))
+	{
+		int pixelDataOffset = *((int*)(bmpFileData + 10));
+		width = *((int*)(bmpFileData + 18));
+		height = *((int*)(bmpFileData + 22));
+		unsigned char* pixelData = bmpFileData + pixelDataOffset;
+		for (int i = 0; i < width * height * 3; i += 3)
+		{
+			// BGR ---> RGB
+			unsigned char temp = pixelData[i];
+			pixelData[i] = pixelData[i + 2];
+			pixelData[i + 2] = temp;
+		}
+		return pixelData;
+	}
+	return nullptr;
+}
--- src/utils.h
+++ src/utils.h
@@ -8,3 +8,5 @@ GLuint CompileShader(GLenum shaderType, const char* shaderCode);
 GLuint CreateProgram(GLuint vsShader, GLuint fsShader);
 
 float GetFrameTime();
+
+unsigned char* DecodeBMP(unsigned char* bmpFileData, int& width, int& height);
