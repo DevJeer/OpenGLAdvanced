f94d49b0e5c32c441173a6f73211eefa70faf228
houjf <18829536923@163.com>
2020-11-05 13:01:59 +0800

    3D模型 解析绘制指令

--- src/model.cpp
+++ src/model.cpp
@@ -8,11 +8,18 @@ Model::Model() {
 // 模型初始化
 void Model::Init(const char* modelPath)
 {
+	// 保存每行的信息 顶点数据源
+	struct FloatData {
+		float v[3];
+	};
+
 	int nFileSize = 0;
 	unsigned char* fileContent = LoadFileContent(modelPath, nFileSize);
 	if (fileContent == nullptr)
 		return;
+	std::vector<FloatData> positions, texcoords, normals;
 	std::stringstream ssFileContent((char*)fileContent);
+	std::string temp;
 	char szOneLine[256];
 	// 流中没有数据了
 	while (!ssFileContent.eof())
@@ -25,18 +32,36 @@ void Model::Init(const char* modelPath)
 		{
 			if (szOneLine[0] == 'v')
 			{
+				std::stringstream ssOneLine(szOneLine);
 				// 当前是纹理坐标
 				if (szOneLine[1] == 't')
 				{
-					printf("texcoord : %s\n", szOneLine);
+					ssOneLine >> temp;
+					FloatData floatData;
+					ssOneLine >> floatData.v[0];
+					ssOneLine >> floatData.v[1];
+					texcoords.push_back(floatData);
+					printf("texcoord : %f, %f\n", floatData.v[0], floatData.v[1]);
 				}
 				else if (szOneLine[1] == 'n') // 当前是法线
 				{
-					printf("normal : %s\n", szOneLine);
+					ssOneLine >> temp;
+					FloatData floatData;
+					ssOneLine >> floatData.v[0];
+					ssOneLine >> floatData.v[1];
+					ssOneLine >> floatData.v[2];
+					normals.push_back(floatData);
+					printf("normal : %f, %f, %f\n", floatData.v[0], floatData.v[1], floatData.v[2]);
 				}
 				else // 当前是物体的坐标位置
 				{
-					printf("position : %s\n", szOneLine);
+					ssOneLine >> temp;
+					FloatData floatData;
+					ssOneLine >> floatData.v[0];
+					ssOneLine >> floatData.v[1];
+					ssOneLine >> floatData.v[2];
+					positions.push_back(floatData);
+					printf("position : %f, %f, %f\n", floatData.v[0], floatData.v[1], floatData.v[2]);
 				}
 			}
 			else if (szOneLine[0] == 'f')  // 当前是索引
